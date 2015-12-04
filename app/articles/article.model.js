c984fac52e000168f630f0b7b745bc69b85e8f84
Scott Hansen <hscott@concur.com>
2015-12-04 14:37:56 -0800

    Expanding page fields for SEO

--- app/articles/article.model.js
+++ app/articles/article.model.js
@@ -27,30 +27,105 @@ var ArticleSchema = new Schema({
         default: '',
         trim: true
     },
-    tags: {
-        type: [{
+    tags: [{
+        type: String,
+    }],
+    seo: {
+        canonical: {
             type: String,
-        }],
+            default: ''
+        },
+        custom_title: {
+            type: String,
+            default: ''
+        }
+        robots: {
+            noarchive: {
+                type: Boolean,
+                default: False
+            },
+            nofollow: {
+                type: Boolean,
+                default: False
+            },
+            noindex: {
+                type: Boolean,
+                default: False
+            },
+            noodp: {
+                type: Boolean,
+                default: False
+            },
+            nosnippet: {
+                type: Boolean,
+                default: False
+            },
+            noydir: {
+                type: Boolean,
+                default: False
+            }
+        }
+    },
+    sitemap: {
+        included: {
+            type: Boolean,
+            default: True
+        },
+        priority: {
+            type: Number,
+            default: .5,
+            min: 0,
+            max: 1,
+        }
+    },
+    redirect: {
+        type: String,
+    },
+    scheduler: {
+        start: {
+            type: Date
+        },
+        end: {
+            type: Date
+        }
+    },
+    images: [{
+        link: String,
+        large_link: String,
+        medium_link: String,
+        small_link: String,
+    }],
+    featured_image: {
+        link: String,
+        large_link: String,
+        medium_link: String,
+        small_link: String,
     },
     url: {
         type: String,
     },
     country_code: {
-        type: [{
-            type: String,
-            enum: regionInfo.listCountries(),
-        }],
+        type: String,
+        enum: regionInfo.listCountries(),
     },
     lang_code: {
-        type: [{
-            type: String,
-            enum: regionInfo.listLanguages(),
-        }],
+        type: String,
+        enum: regionInfo.listLanguages(),
     },
     user: {
         type: Schema.ObjectId,
         ref: 'User'
     }
 }).index({url: 1, country_code: 1, lang_code: 1}, {unique: true});
+
+
+ArticleSchema.pre('save', function(next) {
+    var self = this;
+
+    if(!this.seo['canonical']) {
+        this.seo['canonical'] = this.url;
+    }
+});
+
 
 mongoose.model('Article', ArticleSchema);
